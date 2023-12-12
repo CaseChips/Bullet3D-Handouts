e13a4667d925f04b57c5fce1124970547e3bb8a7
CaseChips <125640305+CaseChips@users.noreply.github.com>
2023-12-12 18:09:03 +0100

    [H-01] DONE!

--- Physics II - Bullet3D - Handout 01/ModuleSceneIntro.cpp
+++ Physics II - Bullet3D - Handout 01/ModuleSceneIntro.cpp
@@ -20,6 +20,10 @@ bool ModuleSceneIntro::Start()
 	// experiment with different camera placements, then use LookAt()
 	// to make it look at the center
 
+	App->camera->Position.y += 1;
+	App->camera->Position.x += 1;
+	App->camera->LookAt(0);
+
 	return ret;
 }
 
@@ -37,9 +41,26 @@ update_status ModuleSceneIntro::Update()
 	// TODO 1: Create a Plane primitive. This uses the plane formula
 	// so you have to express the normal of the plane to create 
 	// a plane centered around 0,0. Make that it draw the axis for reference
+	
+	Plane plane = Plane();
+	
+	plane.axis = true;
+	plane.Render();
 
 	// TODO 6: Draw a sphere of 0.5f radius around the center
 	// Draw somewhere else a cube and a cylinder in wireframe
+
+	Sphere sphere = Sphere();
+	sphere.radius = 0.5f;
+	sphere.Render();
+
+	Cube cube = Cube();
+	cube.SetPos(2, 2, 2);
+	cube.Render();
+
+	Cylinder cylinder = Cylinder();
+	cylinder.SetPos(-2, -2, -2);
+	cylinder.Render();
 
 	return UPDATE_CONTINUE;
 }
